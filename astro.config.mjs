65217b0cfccbd14a613aa32f34d8749ce4d17afd
jhl-hk <valor@jianyuelab.org>
2025-06-15 19:17:42 +0900

    Fix: update link

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://article.jhl.idv.hk/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
